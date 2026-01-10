fb717260e5fb17ad3029cad93c15cd28cacbc632
nicholasphalim <18224098@std.stei.itb.ac.id>
2026-01-11 00:36:47 +0700

    ui: update font & layout of greeting page, add pointing finger image

--- src/app/page.js
+++ src/app/page.js
@@ -8,32 +8,28 @@ import Email from "./components/Email"
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center font-sans px-10">
-      <main className="flex flex-col">
+    <div className="flex flex-col min-h-screen items-center justify-center font-sans px-6">
+      <main className="flex flex-col font-mono">
 
         {/* Intro */}
         <div className="h-screen flex flex-col justify-center ">
-          <div className="text-shadow-lg flex-col flex px-5 text-center">
-            <h1 className="text-2xl font-bold mb-5">Hello World 👋,</h1>
-            <h2 className="text-5xl font-bold leading-13"> <span className="text-2xl">I'm</span> Nicholas Putra Halim</h2>
-            <p className="text-gray-400 mt-3 text-center text-shadow-none">📍Bandung, Indonesia</p>
+          <h1 className="text-2xl text-center font-medium mb-2 ml-5">Hello World 👋</h1>
+          <div className="text-shadow-md flex-col flex p-5 border border-dashed rounded-xl">
+            <h2 className="text-3xl font-bold leading-13"><span className="text-2xl font-medium text-shadow-none">I'm</span> Nicholas Halim</h2>
+            <p className="text-gray-400 text-shadow-none">📍Bandung, Indonesia</p>
+            <p className="text-tertiary mt-3">A second year ITB student studying <b>Information Systems and Technology</b>, with a high interest in Software Development.</p>
           </div>
-          <hr className=" w-full mt-2 transparent border-tertiary"></hr>
           
-          <div className="flex p-3 justify-items-center ">
+
+          <div className="flex p-3 justify-center items-center ">
             <Email></Email>
             <Contacts href="https://github.com/nicholasphalim" src="/github.svg" alt="Github Icon" width={30} height={30}/>
             <Contacts href="https://instagram.com/nicholasphalim" src="/instagram.svg" alt="Instagram Icon"  width={30} height={30}/>
             <Contacts href="https://www.linkedin.com/in/nicholasphalim" src="/linkedin.svg" alt="LinkedIn Icon"  width={30} height={30}/>
           </div>
-          <div className="">
-            <p className="text-center text-tertiary mt-10">A second year ITB student studying <b>Information Systems and Technology</b>, with a high interest in Software Development.</p>
-          </div>
+          <Image src="/pointing.png" height={50} width={50} className="opacity-80 right-5 bottom-0 absolute animate-bounce scr"></Image>
         </div>
 
-        <div className="mt-10">
-          <p>Let's Connect!</p>
-        </div>
 
         {/* Cards */}
         <div className="flex-col flex items-center justify-center">
