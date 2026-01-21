4baf09f40d7b35c81644d6db08ebd6e142819b43
nicholasphalim <18224098@std.stei.itb.ac.id>
2026-01-22 03:14:42 +0700

    refactor: change look of category cards

--- src/app/components/Categories.jsx
+++ src/app/components/Categories.jsx
@@ -1,9 +1,10 @@
 
-export default function Categories({category, description}){
+export default function Categories({category, image, alt, className}){
     return (
-        <div className="border border-gray-400 text-black rounded-2xl font-sans m-3 p-3.5 flex flex-col justify-center items-center shadow-xl">
-            <img alt="Category Picture" src='/nimonscooked.png' className="w-50 rounded-xl"></img>
-            <h1 className="text-md font-medium text-center mt-2">{category}</h1>
+        <div className="relative border rounded-2xl p-4 text-center text-xl border-dashed mx-17 bg-[#D2D4C2]">
+            <img src={image} alt={alt} className={`absolute  ${className}`}></img>
+            <img src="photography.jpg" alt="Category Image"></img>
+            <h1 className="mt-2">{category}</h1>
         </div>
     );
 }
